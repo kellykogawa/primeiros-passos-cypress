2157dda439802094db28fd48d4fb43e07ff5efc6
kellykogawa <kelly19yk@gmail.com>
2024-07-30 18:27:42 -0300

    Add Selectors list for login spec

--- cypress/e2e/loginspec.cy.js
+++ cypress/e2e/loginspec.cy.js
@@ -1,18 +1,27 @@
 describe('Orange HRM Tests', () => {
+
+  const selectorsList = {  //Criar objetos, pois se alguém alterar o username para email por ex, vai ter que alterar o código
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredentialAlert: "[role='alert']"
+  }
+
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') //entrando na página
-    cy.get("[name='username']").type('Admin')  // utilizar o CSS selector para achar atributo único
-    cy.get("[name='password']").type('admin123') // utilizar o CSS selector para achar atributo único
-    cy.get("[type='submit']").click()  // utilizar o CSS selector para achar atributo único
+    cy.get(selectorsList.usernameField).type('Admin')  // utilizar o CSS selector para achar atributo único
+    cy.get(selectorsList.passwordField).type('admin123') // utilizar o CSS selector para achar atributo único
+    cy.get(selectorsList.loginButton).click()  // utilizar o CSS selector para achar atributo único
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //para verificar que será redirecionado a pagina correta, copiar o caminho da URL após .com
-    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard') // na página principal, para saber se está correto, o atributo contendo a palavra "dashboard"
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard') // na página principal, para saber se está correto, o atributo contendo a palavra "dashboard"
   })
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Bruno')
-    cy.get("[name='password']").type('Test')
-    cy.get("[type='submit']").click()
-    cy.get("[role='alert']")
+    cy.get(selectorsList.usernameField).type('Test')
+    cy.get(selectorsList.passwordField).type('Test')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
   
   })
 })
