b34be8a0cabb769ac5295142dc591d6a799f3473
kellykogawa <kelly19yk@gmail.com>
2024-08-01 11:31:09 -0300

    Adding user data json

--- cypress/e2e/loginspec.cy.js
+++ cypress/e2e/loginspec.cy.js
@@ -1,25 +1,28 @@
+import userData from '../fixtures/userData.json'
+
 describe('Orange HRM Tests', () => {
 
-  const selectorsList = {  //Criar objetos, pois se alguém alterar o username para email por ex, vai ter que alterar o código
+  const selectorsList = {  
     usernameField: "[name='username']",
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredentialAlert: "[role='alert']"
   }
 
   it('Login - Success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') //entrando na página
-    cy.get(selectorsList.usernameField).type('Admin')  // utilizar o CSS selector para achar atributo único
-    cy.get(selectorsList.passwordField).type('admin123') // utilizar o CSS selector para achar atributo único
-    cy.get(selectorsList.loginButton).click()  // utilizar o CSS selector para achar atributo único
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //para verificar que será redirecionado a pagina correta, copiar o caminho da URL após .com
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard') // na página principal, para saber se está correto, o atributo contendo a palavra "dashboard"
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') 
+    cy.get(selectorsList.usernameField).type(userData.userSucess.username)  
+    cy.get(selectorsList.passwordField).type(userData.userSucess.password) 
+    cy.get(selectorsList.loginButton).click()  
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index') 
+    cy.get(selectorsList.dashboardGrid) 
   })
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Test')
-    cy.get(selectorsList.passwordField).type('Test')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.wrongCredentialAlert)
   
