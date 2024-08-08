08915936971674b9fb2e4d23432ca6d0129e4917
kellykogawa <kelly19yk@gmail.com>
2024-08-08 16:27:10 -0300

    add random values on user spec chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -5,6 +5,9 @@ import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/myInfoPage'
 import BuzzPage from '../pages/buzzPage'
 
+const Chance = require('chance');
+
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
@@ -21,13 +24,13 @@ describe('Orange HRM Tests', () => {
 
     menuPage.accessMyInfo()
 
-    myinfoPage.fillPersonalDetails('First Name', 'Middle Name', 'Last Name')
-    myinfoPage.fillEmployeeDetails('000000', '6789', '54321', '2025-08-22')
+    myinfoPage.fillPersonalDetails(chance.first(), chance.word(), chance.last())
+    myinfoPage.fillEmployeeDetails(chance.natural({min: 1000 , max: 9999}), chance.natural({min: 1000 , max: 9999}), chance.natural({min: 1000 , max: 9999}), '2050-11-25')
     myinfoPage.fillStatus('2000-05-01')
     myinfoPage.saveForm()
 
     menuPage.accessBuzz()
     
-    buzzPage.postNews('Hii')
+    buzzPage.postNews(chance.sentence({ words: 5 }))
   })
 })
